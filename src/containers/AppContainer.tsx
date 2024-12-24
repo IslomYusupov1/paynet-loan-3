02a2bf16fc95fbff4780701bb91c2a73bdbe4915
Islom Yusupov <islom.yusupov07031997@gmail.com>
2024-12-24 12:26:35 +0500

    init project

--- src/containers/AppContainer.tsx
+++ src/containers/AppContainer.tsx
@@ -6,12 +6,11 @@ function AppContainer() {
     useEffect(() => {
         const userAgent = navigator.userAgent || navigator.vendor;
         const headerElement: any = document.querySelector('.main-height-container')
-
         if (!headerElement) return; // Проверить, существует ли элемент
 
         if (/iPhone|iPad|iPod/i.test(userAgent)) {
             // Проверяем, поддерживается ли env(safe-area-inset-top)
-            let safeAreaTop = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('env(safe-area-inset-top)')) || 44; // Если значение не возвращается, используем 44
+            let safeAreaTop = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('env(safe-area-inset-top)')) || 100; // Если значение не возвращается, используем 44
             headerElement.style.paddingTop = `${safeAreaTop}px`;
         } else {
             // Android: фиксированный расчет
