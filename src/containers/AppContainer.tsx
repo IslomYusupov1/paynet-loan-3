42200a9a90d62bcea3ad4bbabea6940d04218900
Islom Yusupov <islom.yusupov07031997@gmail.com>
2024-12-24 14:07:58 +0500

    init project

--- src/containers/AppContainer.tsx
+++ src/containers/AppContainer.tsx
@@ -1,6 +1,20 @@
 import RootContainer from "./RootContainer.tsx";
+import {useEffect} from "react";
 
 function AppContainer() {
+    useEffect(() => {
+        const userAgent = navigator.userAgent || navigator.vendor;
+        const headerElement: any = document.querySelector('.main-height-container')
+        if (!headerElement) return;
+
+        if (/iPhone|iPad|iPod/i.test(userAgent)) {
+            //@ts-ignore
+            let safeAreaTop = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('env(safe-area-inset-top)', 50));
+            headerElement.style.paddingTop = `${safeAreaTop}px`;
+        } else {
+            headerElement.style.paddingTop = `24px`;
+        }
+    }, []);
     return (
         <RootContainer/>
     );
