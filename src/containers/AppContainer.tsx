412830519ee415ea2788253416b2232ea958d1e4
Islom Yusupov <islom.yusupov07031997@gmail.com>
2024-12-24 12:31:40 +0500

    init project

--- src/containers/AppContainer.tsx
+++ src/containers/AppContainer.tsx
@@ -1,24 +1,6 @@
 import RootContainer from "./RootContainer.tsx";
-import {useEffect} from "react";
 
 function AppContainer() {
-    // padding-top: env(safe-area-inset-top, 44px);
-    useEffect(() => {
-        const userAgent = navigator.userAgent || navigator.vendor;
-        const headerElement: any = document.querySelector('.main-height-container')
-        if (!headerElement) return; // Проверить, существует ли элемент
-
-        if (/iPhone|iPad|iPod/i.test(userAgent)) {
-            // Проверяем, поддерживается ли env(safe-area-inset-top)
-            //@ts-ignore
-            let safeAreaTop = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('env(safe-area-inset-top)', 44)); // Если значение не возвращается, используем 44
-            headerElement.style.paddingTop = `${safeAreaTop}px`;
-        } else {
-            // Android: фиксированный расчет
-            const statusBarHeight = 24 * window.devicePixelRatio; // Примерное значение
-            headerElement.style.paddingTop = `${statusBarHeight}px`;
-        }
-    }, []);
     return (
         <RootContainer/>
     );
